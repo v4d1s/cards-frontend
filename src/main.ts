0c1e3ef148f1c2a043405482fba7e38686ab0f00
Vadim Kulikov <kylukov2003@yandex.ru>
2024-04-08 14:26:07 +0300

    Add and enable VaTeX

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueLatex from "vatex";
 
 import BootstrapVue3 from "bootstrap-vue-3";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
 
-createApp(App).use(BootstrapVue3).use(store).use(router).mount("#app");
+createApp(App)
+  .use(VueLatex)
+  .use(BootstrapVue3)
+  .use(store)
+  .use(router)
+  .mount("#app");
